87907f275a7295d7b7b167c8b5437bde97e1f644
Tamás Russ <github@tamasruss.com>
2025-07-09 23:01:12 +0200

    infra: add linting to CD pipeline
    also run formatter

--- packages/shell/src/App.vue
+++ packages/shell/src/App.vue
@@ -1,7 +1,5 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-	<h1 class="text-5xl font-bold text-center my-8 text-indigo-600 uppercase tracking-wider">SET</h1>
-
+  <h1 class="text-5xl font-bold text-center my-8 text-indigo-600 uppercase tracking-wider">SET</h1>
 </template>
