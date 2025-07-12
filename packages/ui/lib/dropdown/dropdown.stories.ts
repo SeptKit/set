14c8cea0729ace9ed2f6af654dff9347041e8a5e
Tamás Russ <github@tamasruss.com>
2025-07-12 20:51:07 +0200

    chore:  run format

--- packages/ui/lib/dropdown/dropdown.stories.ts
+++ packages/ui/lib/dropdown/dropdown.stories.ts
@@ -3,65 +3,84 @@ import { expect } from 'storybook/test'
 // COMPONENTS
 import Dropdown from './dropdown.vue'
 
-
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
-	title: 'Actions/Dropdown',
-	component: Dropdown,
-	parameters: {
-		// Set the height of the story canvas
-		layout: 'padded',
-	},
-	// This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
-	tags: ['autodocs'],
-	argTypes: {
-		// slots
-		label: {
-			control: 'text'
-		},
-		items: {
-			control: 'text'
-		},
-		// props
-		color: { control: { type: 'select', labels: { default: '' } }, options: ['default', 'neutral', 'primary', 'secondary', 'accent', 'info', 'success', 'warning', 'error'] },
-		variant: { control: { type: 'select', labels: { default: '' } }, options: ['default', 'outline', 'dash', 'soft', 'ghost', 'link'] },
-		size: { control: { type: 'select', labels: { default: '' } }, options: ['default', 'xs', 'sm', 'md', 'lg', 'xl'] },
-		active: { control: 'boolean' },
-		disabled: { control: 'boolean' },
-		modifier: { control: { type: 'select', labels: { default: '' } }, options: ['default', 'wide', 'block', 'square', 'circle'] }
-	},
+  title: 'Actions/Dropdown',
+  component: Dropdown,
+  parameters: {
+    // Set the height of the story canvas
+    layout: 'padded',
+  },
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  argTypes: {
+    // slots
+    label: {
+      control: 'text',
+    },
+    items: {
+      control: 'text',
+    },
+    // props
+    color: {
+      control: { type: 'select', labels: { default: '' } },
+      options: [
+        'default',
+        'neutral',
+        'primary',
+        'secondary',
+        'accent',
+        'info',
+        'success',
+        'warning',
+        'error',
+      ],
+    },
+    variant: {
+      control: { type: 'select', labels: { default: '' } },
+      options: ['default', 'outline', 'dash', 'soft', 'ghost', 'link'],
+    },
+    size: {
+      control: { type: 'select', labels: { default: '' } },
+      options: ['default', 'xs', 'sm', 'md', 'lg', 'xl'],
+    },
+    active: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+    modifier: {
+      control: { type: 'select', labels: { default: '' } },
+      options: ['default', 'wide', 'block', 'square', 'circle'],
+    },
+  },
 } satisfies Meta<typeof Dropdown>
 
 export default meta
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-	args: {
-		label: 'Click me',
-		items: `
+  args: {
+    label: 'Click me',
+    items: `
 			<li><a href="#">Item 1</a></li>
 			<li><a href="#">Item 2</a></li>
-		`
-	},
-	render: (args) => ({
-		components: { Dropdown },
-		setup() {
-			return { args }
-		},
-		template: `
+		`,
+  },
+  render: (args) => ({
+    components: { Dropdown },
+    setup() {
+      return { args }
+    },
+    template: `
 			<Dropdown v-bind="args">
 				<template #label>{{ args.label }}</template>
 				<template #items>
 					<div v-html="args.items"></div>
 				</template>
 			</Dropdown>
-		`
-	}),
-	play: async ({ canvas, userEvent }) => {
-		await userEvent.click(canvas.getByTestId('dropdown-trigger'));
+		`,
+  }),
+  play: async ({ canvas, userEvent }) => {
+    await userEvent.click(canvas.getByTestId('dropdown-trigger'))
 
-		await expect(
-			canvas.getByTestId('dropdown-wrapper')
-		).toHaveAttribute('open', '')
-	},
-};
+    await expect(canvas.getByTestId('dropdown-wrapper')).toHaveAttribute('open', '')
+  },
+}
