d01e79b5f9d45315d022b4c22458059923e0f0eb
Nikolaj Stuckmann <n.stuckmann@MacBook-Pro-von-Nikolaj.local>
2025-07-15 13:53:20 +0200

    feat(shell): fix app test

--- packages/shell/src/app.test.ts
+++ packages/shell/src/app.test.ts
@@ -2,8 +2,8 @@ import { render } from 'vitest-browser-vue'
 import { expect, test } from 'vitest'
 import app from './app.vue'
 
-test('renders the headline "SET"', async () => {
+test('renders the line "main area"', async () => {
 	const screen = render(app)
 
-	await expect.element(screen.getByText('SET')).toBeVisible()
+	await expect.element(screen.getByText('main area')).toBeVisible()
 })
