b354d4acbc640573ff8a0e1192f59e8e9c6fddfb
Nikolaj Stuckmann <n.stuckmann@MacBook-Pro-von-Nikolaj.local>
2025-07-25 11:07:34 +0200

    feat(monorepo): fix missing d.ts for build

--- packages/sclsdk/vite.config.ts
+++ packages/sclsdk/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
 	build: {
+		emptyOutDir: false,
 		sourcemap: import.meta.env?.DEV,
 		lib: {
 			entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
