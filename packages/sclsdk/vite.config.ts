c6df2b1e5a974088127a43abd0b10389a2a3c8d7
Nikolaj Stuckmann <n.stuckmann@MacBook-Pro-von-Nikolaj.local>
2025-07-25 09:10:25 +0200

    feat(monorepo): set up scl-sdk structure

---
+++ packages/sclsdk/vite.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite/client" />
+import { fileURLToPath, URL } from 'node:url'
+// VITE
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+	build: {
+		sourcemap: import.meta.env?.DEV,
+		lib: {
+			entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+			fileName: 'index',
+			name: 'SCLSDK',
+			formats: ['es'],
+		},
+	},
+})
