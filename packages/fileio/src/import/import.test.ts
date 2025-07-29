eaf2287e7ec07ee1b4b691ea3e57cb9e9e43b91c
Michel Guerin <dev@guerin.at>
2025-07-29 22:46:57 +0200

    feat(fileio): add new test cases for import

--- packages/fileio/src/import/import.test.ts
+++ packages/fileio/src/import/import.test.ts
@@ -1,109 +1,127 @@
 import { describe, expect, it } from 'vitest'
+import Dexie from 'dexie'
+// FUNCTION TO TEST
 import { importXmlFiles } from './import.main'
+// HANDLERS
+import { handleExpectedElementCounts, handleExpectedRelationships } from './import.test.handler'
+// TYPES
+import type { TestCase } from './import.test.types'
+import { DatabaseInstance } from '@/common/common.types'
 
 describe('Import', () => {
 	describe('Feature', () => {
-		type TestCase = {
-			desc: string
-			fileContent: string
-			expectedFileName: string
-			expectedElementCounts: { [tableName: string]: number }
-		}
+		const randomUUID = crypto.randomUUID()
 
 		const featureTests: TestCase[] = [
 			{
-				desc: 'first test',
+				description: 'Test with a single IED element',
+				fileName: `test1-${randomUUID}.scd`,
 				fileContent: '<SCL><IED name="just the one"></IED></SCL>',
-				expectedFileName: 'test',
+				expectedFileName: `test1-${randomUUID}`,
 				expectedElementCounts: {
 					IED: 1,
 				},
 			},
 			{
-				desc: 'second test',
-				fileContent: `<SCL xmlns="http://www.iec.ch/61850/2003/SCL" xmlns:eIEC61850-6-100="http://www.iec.ch/61850/2019/SCL/6-100" version="2007" revision="C" release="5">
+				description: 'Test with a new element with namespace',
+				fileName: `test2-${randomUUID}.scd`,
+				fileContent: `
+					<SCL xmlns="http://www.iec.ch/61850/2003/SCL" xmlns:eIEC61850-6-100="http://www.iec.ch/61850/2019/SCL/6-100" version="2007" revision="C" release="5">
 						<IED name="just the one"></IED>
 						<eIEC61850-6-100:NewElementWithNamespace>with a text</eIEC61850-6-100:NewElementWithNamespace>
-					</SCL>`,
-				expectedFileName: 'test',
+					</SCL>
+				`,
+				expectedFileName: `test2-${randomUUID}`,
 				expectedElementCounts: {
 					IED: 1,
 					NewElementWithNamespace: 1,
+				},
+			},
+			{
+				description: 'Test with multiple IED elements',
+				fileName: `test3-${randomUUID}.scd`,
+				fileContent: `
+					<SCL>
+						<IED name="first"></IED>
+						<IED name="second"></IED>
+					</SCL>
+				`,
+				expectedFileName: `test3-${randomUUID}`,
+				expectedElementCounts: {
+					IED: 2,
+				},
+			},
+			{
+				description: 'Test with empty SCL element',
+				fileName: `test4-${randomUUID}.scd`,
+				fileContent: '<SCL></SCL>',
+				expectedFileName: `test4-${randomUUID}`,
+				expectedElementCounts: {
+					SCL: 1,
+				},
+			},
+			{
+				description: 'Test with empty file content',
+				fileName: `test5-${randomUUID}.scd`,
+				fileContent: '',
+				expectedFileName: `test5-${randomUUID}`,
+				expectedElementCounts: undefined,
+			},
+			{
+				description: 'Test parent and child relationships',
+				fileName: `test1-${randomUUID}.scd`,
+				fileContent: '<SCL uuid="123"><IED uuid="456" name="just the one"></IED></SCL>',
+				expectedFileName: `test1-${randomUUID}`,
+				expectedElementCounts: {
+					SCL: 1,
+					IED: 1,
+				},
+				expectedRelationships: {
+					SCL: [
+						{
+							uuid: '123',
+							parent: null,
+							children: [{ uuid: '456', tagName: 'IED' }],
+						},
+					],
+					IED: [
+						{
+							uuid: '456',
+							parent: { uuid: '123', tagName: 'SCL' },
+							children: [],
+						},
+					],
 				},
 			},
 		]
 
 		featureTests.forEach(testFeature)
 
-		function testFeature(tc: TestCase) {
-			it(tc.desc, async () => {
+		function testFeature(testCase: TestCase) {
+			it(testCase.description, async () => {
 				// Arrange
-				const file = new File([tc.fileContent], 'test.scd', {
+				const file = new File([testCase.fileContent], testCase.fileName, {
 					type: 'text/plain',
 				})
 
 				// Act
 				const fileNames = await importXmlFiles({ files: [file] })
 
-				// Assert
-				expect(fileNames).toContain(tc.expectedFileName)
+				const databaseInstance = new Dexie(testCase.expectedFileName) as DatabaseInstance
 
-				const db = await openDatabase('test')
-				for (const tableName of Object.keys(tc.expectedElementCounts)) {
-					const expectedCount = tc.expectedElementCounts[tableName]
-					const nrOfElements = await getElementCountOfTable(db, tableName)
-					expect(nrOfElements).toEqual(expectedCount)
+				// Assert
+				if (testCase.expectedElementCounts)
+					await handleExpectedElementCounts(databaseInstance, fileNames, testCase)
+				else await expect(databaseInstance.open()).rejects.toThrow(Dexie.NoSuchDatabaseError)
+
+				if (testCase.expectedRelationships) {
+					await handleExpectedRelationships(databaseInstance, testCase)
 				}
 
 				// Cleanup
-				db.close()
-				await deleteDatabase('test')
+				databaseInstance.close()
+				await databaseInstance.delete()
 			})
 		}
 	})
 })
-
-function openDatabase(dbName: string): Promise<IDBDatabase> {
-	return new Promise((resolve, reject) => {
-		const request = indexedDB.open(dbName)
-
-		request.onerror = () => reject(request.error)
-		request.onsuccess = () => resolve(request.result)
-
-		request.onupgradeneeded = (event) => {
-			const db = (event.target as IDBOpenDBRequest).result
-			// Create object stores if they don't exist
-			if (!db.objectStoreNames.contains(dbName)) {
-				// throw new Error(JSON.stringify({ msg: 'database does not exist', dbName }))
-			}
-		}
-	})
-}
-
-async function deleteDatabase(dbName: string): Promise<void> {
-	const dbs = await indexedDB.databases()
-	const hasDB = Boolean(dbs.find((db) => db.name === dbName))
-	if (!hasDB) {
-		return
-	}
-
-	return new Promise((resolve, reject) => {
-		const request = indexedDB.deleteDatabase(dbName)
-		request.onerror = () => reject(request.error)
-		request.onsuccess = () => resolve()
-	})
-}
-
-function getElementCountOfTable(db: IDBDatabase, tableName: string): Promise<number> {
-	return new Promise((resolve, reject) => {
-		// const tables = Array.from(db.objectStoreNames)
-		// console.debug({ level: 'debug', msg: 'getelcount', tableName, tables })
-
-		const transaction = db.transaction(tableName, 'readonly')
-		const store = transaction.objectStore(tableName)
-		const request = store.count()
-
-		request.onerror = () => reject(request.error)
-		request.onsuccess = () => resolve(request.result)
-	})
-}
