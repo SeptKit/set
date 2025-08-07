91a6bf2ecd936ded37f7f60bd46c829e323cf912
Nikolaj Stuckmann <n.stuckmann@Mac.fritz.box>
2025-08-07 08:48:18 +0200

    feat(extensions): init dataflow extension by using the template

---
+++ packages/extensions/dataflow/README.md
@@ -0,0 +1,21 @@
+# Extension Template
+
+This is a template for making SET Extensions.
+
+# Starting the Template
+
+pnpm build:serve
+
+# Creating a new Extension
+
+-Copy the `_template` folder
+-Change `__Template__` in following files:
+
+- app.vue
+- app.test.ts
+- package.json
+- index.html
+
+-Register the new Extension in:
+
+-main-area.vue -> dummyExtensionLocationList
---
+++ packages/extensions/dataflow/src/app.vue
@@ -0,0 +1,44 @@
+<template>
+	<Layout>
+		<div
+			class="min-h-screen flex flex-col items-center justify-center"
+			:style="{
+				background: 'repeating-linear-gradient(45deg, #ffe066, #ffe066 30px, #fff 30px, #fff 60px)',
+			}"
+		>
+			<h1 class="text-5xl font-bold text-center my-8 uppercase tracking-wider">Dataflow Extension</h1>
+			<h2 class="text-2xl text-center font-semibold my-4">Based on Template for SET Extensions</h2>
+			<h2 class="text-2xl text-center font-semibold my-4">file: {{ fileName }}</h2>
+		</div>
+	</Layout>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
+
+const props = defineProps<{
+	api: { [key: string]: any }
+}>()
+
+let fileName = ref('')
+let unsubscribe = () => {}
+
+onMounted(() => {
+	unsubscribe = props.api.activeFileName.subscribe((newFile: string, oldFile: any) => {
+		fileName.value = newFile
+	})
+})
+
+onUnmounted(() => {
+	unsubscribe()
+})
+</script>
+
+<style scoped>
+/*
+	We import it here and not on the top so it is scoped and does not affect
+	the rest of the ui
+*/
+
+@reference "@/assets/main.css";
+</style>
