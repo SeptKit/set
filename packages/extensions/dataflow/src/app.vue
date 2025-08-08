eb8ad062c309b870e99d92e6b6c597b926c103f6
Nikolaj Stuckmann <n.stuckmann@Mac.fritz.box>
2025-08-08 11:58:05 +0200

    feat(extensions): init vue-flow

--- packages/extensions/dataflow/src/app.vue
+++ packages/extensions/dataflow/src/app.vue
@@ -6,15 +6,21 @@
 				background: 'repeating-linear-gradient(45deg, #ffe066, #ffe066 30px, #fff 30px, #fff 60px)',
 			}"
 		>
-			<h1 class="text-5xl font-bold text-center my-8 uppercase tracking-wider">Dataflow Extension</h1>
+			<h1 class="text-5xl font-bold text-center my-8 uppercase tracking-wider">
+				Dataflow Extension
+			</h1>
 			<h2 class="text-2xl text-center font-semibold my-4">Based on Template for SET Extensions</h2>
-			<h2 class="text-2xl text-center font-semibold my-4">file: {{ fileName }}</h2>
+			<!-- <h2 class="text-2xl text-center font-semibold my-4">file: {{ fileName }}</h2> -->
 		</div>
 	</Layout>
+	<div class="flex flex-col items-center justify-center">
+		<DataflowVisualisation :api="api" />
+	</div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue'
+import DataflowVisualisation from './components/dataflow-visualisation.vue'
 
 const props = defineProps<{
 	api: { [key: string]: any }
---
+++ packages/extensions/dataflow/src/components/dataflow-visualisation.vue
@@ -0,0 +1,89 @@
+<template>
+	<div>
+		<Dropdown>
+			<template>
+				<dropdown>
+					<template #label>Click me</template>
+					<template #items>
+						<li>Item 1</li>
+						<li>Item 2</li>
+					</template>
+				</dropdown>
+			</template>
+		</Dropdown>
+		<Dropdown>
+			<template>
+				<dropdown>
+					<template #label>Click me</template>
+					<template #items>
+						<li>Item 1</li>
+						<li>Item 2</li>
+					</template>
+				</dropdown>
+			</template>
+		</Dropdown>
+	</div>
+	<!-- LNs -->
+	<div style="width: 100%; height: 500px; position: relative">
+		<VueFlow
+			v-model="elements"
+			:fit-view-on-init="false"
+			:nodes-draggable="false"
+			:node-types="nodeTypes"
+			:extent="'parent'"
+			style="width: 100%; height: 100%"
+		>
+			<Background pattern-color="light-grey" :gap="16" />
+		</VueFlow>
+	</div>
+	<button>end</button>
+</template>
+
+<script setup lang="ts">
+/*
+	-dropdowns for selecting LNs (props: xml?, callback: activeLN?)
+	-2x LN (props: visible?, label, portsSide, ports: [], )
+	-Button (+), visible?
+*/
+
+import { Dropdown } from '@septkit/ui'
+import customNode from './custom-node.vue'
+import { ref } from 'vue'
+import { VueFlow, type NodeTypesObject } from '@vue-flow/core'
+import { Background } from '@vue-flow/background'
+
+const props = defineProps<{
+	api: { [key: string]: any }
+}>()
+
+const nodeTypes: NodeTypesObject = { custom: customNode as NodeTypesObject }
+
+const nodeStyles = {
+	width: '200px',
+	height: '300px',
+	border: '2px solid black',
+	//zIndex: 10,
+	padding: '3px',
+}
+
+const elements = ref([
+	{
+		id: 'a',
+		type: 'custom',
+		data: { label: 'LNode A' },
+		position: { x: 200, y: 0 },
+		style: nodeStyles,
+	},
+	{
+		id: 'b',
+		type: 'custom',
+		data: { label: 'LNode B' },
+		position: { x: 600, y: -300 },
+		style: nodeStyles,
+	},
+])
+</script>
+
+<style scoped>
+@reference "@/assets/main.css";
+</style>
