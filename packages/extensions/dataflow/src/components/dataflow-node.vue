013879559ebad06ba9862777cbef98d35f21b5d3
Nikolaj Stuckmann <n.stuckmann@Mac.fritz.box>
2025-08-11 17:28:07 +0200

    feat(extensions): init structure without vue-flow

---
+++ packages/extensions/dataflow/src/components/dataflow-node.vue
@@ -0,0 +1,51 @@
+<template>
+	<div style="display: flex; flex-direction: column; align-items: center; justify-content: center">
+		<!-- here will live a dropdown to select the LN that should be displayed -->
+		<div class="dataflow-dropdown">
+			<Dropdown>
+				<template #label>Dropdown</template>
+				<template #items>
+					<li>Item 1</li>
+					<li>Item 2</li>
+				</template>
+			</Dropdown>
+		</div>
+
+		<!-- here will live a LN that can take props to display the correct amount of connections-->
+		<div class="dataflow-node" style="width: 200px; height: 200px; background-color: orange">
+			Logical Node
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { Dropdown } from '@septkit/ui'
+
+const props = defineProps(['type', 'data'])
+</script>
+
+<style scoped>
+.dataflow-dropdown {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+.dataflow-node {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 200px;
+	height: 100px;
+	border: 1px solid #ccc;
+	border-radius: 8px;
+	background-color: #f9f9f9;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	margin: 10px;
+	font-size: 16px;
+	color: #333;
+	text-align: center;
+	cursor: pointer;
+}
+</style>
--- packages/extensions/dataflow/src/components/dataflow-visualisation.vue
+++ packages/extensions/dataflow/src/components/dataflow-visualisation.vue
@@ -1,89 +1,35 @@
 <template>
-	<div>
-		<Dropdown>
-			<template>
-				<dropdown>
-					<template #label>Click me</template>
-					<template #items>
-						<li>Item 1</li>
-						<li>Item 2</li>
-					</template>
-				</dropdown>
-			</template>
-		</Dropdown>
-		<Dropdown>
-			<template>
-				<dropdown>
-					<template #label>Click me</template>
-					<template #items>
-						<li>Item 1</li>
-						<li>Item 2</li>
-					</template>
-				</dropdown>
-			</template>
-		</Dropdown>
+	<div
+		class="node-container"
+		style="display: flex; flex-direction: row; align-items: center; justify-content: center"
+	>
+		<dataflowNode type="source" />
+
+		<!-- connection Component here? -->
+		<div>- Connections -</div>
+
+		<dataflowNode type="receiver" />
 	</div>
-	<!-- LNs -->
-	<div style="width: 100%; height: 500px; position: relative">
-		<VueFlow
-			v-model="elements"
-			:fit-view-on-init="false"
-			:nodes-draggable="false"
-			:node-types="nodeTypes"
-			:extent="'parent'"
-			style="width: 100%; height: 100%"
-		>
-			<Background pattern-color="light-grey" :gap="16" />
-		</VueFlow>
-	</div>
-	<button>end</button>
+
+	<!-- "Plus-button" component with modal for new conncetion here? -->
 </template>
 
 <script setup lang="ts">
-/*
-	-dropdowns for selecting LNs (props: xml?, callback: activeLN?)
-	-2x LN (props: visible?, label, portsSide, ports: [], )
-	-Button (+), visible?
-*/
-
-import { Dropdown } from '@septkit/ui'
-import customNode from './custom-node.vue'
-import { ref } from 'vue'
-import { VueFlow, type NodeTypesObject } from '@vue-flow/core'
-import { Background } from '@vue-flow/background'
+import dataflowNode from './dataflow-node.vue'
 
 const props = defineProps<{
 	api: { [key: string]: any }
 }>()
-
-const nodeTypes: NodeTypesObject = { custom: customNode as NodeTypesObject }
-
-const nodeStyles = {
-	width: '200px',
-	height: '300px',
-	border: '2px solid black',
-	//zIndex: 10,
-	padding: '3px',
-}
-
-const elements = ref([
-	{
-		id: 'a',
-		type: 'custom',
-		data: { label: 'LNode A' },
-		position: { x: 200, y: 0 },
-		style: nodeStyles,
-	},
-	{
-		id: 'b',
-		type: 'custom',
-		data: { label: 'LNode B' },
-		position: { x: 600, y: -300 },
-		style: nodeStyles,
-	},
-])
 </script>
 
 <style scoped>
 @reference "@/assets/main.css";
+
+.node-container {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 1rem;
+}
 </style>
