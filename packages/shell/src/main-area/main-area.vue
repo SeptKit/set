310df7816c682825db1a575a4c80b1d305ec9d46
Eva Ngo <e.ngo@sprinteins.com>
2025-08-21 15:59:40 +0200

    chore: upgrade extension versions and shell version

--- packages/shell/src/main-area/main-area.vue
+++ packages/shell/src/main-area/main-area.vue
@@ -21,8 +21,8 @@ import type { Optional } from '../x/types'
 const store = useMainAreaWidgetStore()
 
 const extensionList = [
-	'https://septkit.github.io/extensions/structure/2/', // Structure Prod
-	'https://septkit.github.io/extensions/dataflow/1/', // Dataflow Prod
+	'https://septkit.github.io/extensions/structure/3/', // Structure Prod
+	'https://septkit.github.io/extensions/dataflow/2/', // Dataflow Prod
 	// 'http://localhost:54945/', //Structure DEV
 	// 'http://127.0.0.1:51003/',
 	// 'http://localhost:54944/',
