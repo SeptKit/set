9bff40ec44426baadedbf6cb5c12aaaf41449dde
Eva Ngo <e.ngo@sprinteins.com>
2025-08-28 15:38:06 +0200

    Revert "test: add button2 for testing"
    This reverts commit b3c0b10151bc5d48753ad61a19e599f74e967a9c.

--- packages/ui/lib/index.ts
+++ packages/ui/lib/index.ts
@@ -1,7 +1,6 @@
 // ACTIONS
 export { default as Button } from './button/button.vue'
 export { default as Dropdown } from './dropdown/dropdown.vue'
-export { default as Button2 } from './button2/button2.vue'
 // DATA DISPLAY
 // NAVIGATION
 // FEEDBACK
